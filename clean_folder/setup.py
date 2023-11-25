2eee90d5b5dcd3fc0358e4a21e075da0fd57697d
Kollos777 <kolloslit@gmail.com>
2023-11-25 16:33:18 -0600

    add clean_folder

---
+++ clean_folder/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(
+   name='clean_folder',
+   version='1.0',
+   description='A useful module',
+   url="https://github.com/Kollos777/Folder_sorting.git",
+   author='Kostiantyn Litvinov',
+   author_email='Kolloslit@gmail.com',
+   license='MIT',
+   packages=['clean_folder'],
+   install_requires=[],
+   entry_points={
+       'console_scripts': [
+           'clean_folder = clean_folder.clean: main'
+       ]
+   }
+)
